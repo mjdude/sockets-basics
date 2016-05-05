26f4b51b8fff6983dd3c58b0cf818d7dcfdc8f45
mjdude <mo@syncspot.net>
2016-05-05 16:41:31 +0100

    add message to browser UI

--- server.js
+++ server.js
@@ -11,7 +11,8 @@ io.on('connection', function(socket){
 
     socket.on('message', function(message){
       console.log('Message Recieved: ' + message.text);
-      socket.broadcast.emit('message' , message);
+
+      io.emit('message' , message);
 
     });
 
